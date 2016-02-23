d108804a076f920c732fdc644e6d63bf7ac021dd
arush-sal <arush.sal@gmail.com>
2016-02-23 07:35:20 +0530

    Fixed issue no #1, and did lots of other changes mentioned in description.
    The reason behind the issue was differnce between vagrant's ubuntu flavor and regular ubuntu flavor, or it maybe a result of difference in Ubuntu14.04's repositry and Ubuntu 15.10's repositry.
    Apart from that many other changes such as, color formatting, conditional file changes have been added.
    Now, the script will check for database if exists, not duplicate entry will be made in /etc/hosts or in nginx's default configuration file and mysql_install_db has been removed(wasn't able to find an apt solution).

--- wp-nginx.sh
+++ wp-nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-nginx_home="/var/www/html/"
+nginx_home="/usr/share/nginx/html"
 wp_latest="http://wordpress.org/latest.zip"
 wp_opt="/tmp/wordepress_latest.zip"
-wp="${nginx_home}wordpress/"
+wp="${nginx_home}/wordpress"
 msg="Last command exited with non 0 status. Exiting."
 date="$(stat -c %y /var/lib/apt/periodic/update-success-stamp | cut -d' ' -f1)"
 lastD="$(date -d $date +'%Y%m%d')"
@@ -24,7 +24,7 @@ php='error_page 404 /404.html;
 exitfn(){
 if (( $? )); then
 echo
-echo "$msg"
+echo -e "\e[31m$msg\e[30m"
 echo
 exit 1
 fi
@@ -32,12 +32,12 @@ fi
 install(){
         if hash $1 2> /dev/null; then
                 echo
-                echo "$1 Installed. Continuing..."
+                echo -e '\e[32m'"$1 Installed. Continuing...\e[30m"
                 echo
                 sleep 2
         else
                 echo
-                echo "$1 not found. Installing..."
+                echo -e '\e[31m'"$1 not found. Installing...\e[30m"
                 echo
                 sleep 2
                 sudo apt-get install $2 -y
@@ -47,32 +47,53 @@ install(){
 }
 
 if [[ $lastD -ne $currentD ]]; then
-sudo apt-get update
+	sudo apt-get update
 fi
 install nginx nginx
 install mysql mysql-server
-[[ -e /usr/my.cnf ]] && echo "MySQL System Tables Installed. Continuing..." || sudo mysql_install_db
-[[ -e /usr/share/php5/mysql/mysql.ini ]] && echo "php5-mysql Installed. Continuing..." || install php5-mysql php5-mysql
+[[ -e /usr/share/php5/mysql/mysql.ini ]] && echo -e "\e[32mphp5-mysql Installed. Continuing...\e[30m" || install php5-mysql php5-mysql
 install php5-fpm php5-fpm
 
 cp /etc/php5/fpm/php.ini /tmp/php.tmp
 sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /tmp/php.tmp
 sudo mv /tmp/php.tmp /etc/php5/fpm/php.ini
-echo "Restarting php5-fpm service"
+echo
+echo -e "\e[34mRestarting php5-fpm service\e[30m"
+echo
 sudo service php5-fpm restart
+sleep 2
 
 echo
-echo "Please enter your domain name: "
-echo
+echo -e "Please enter your domain name.\n\e[31m\e[1mDO NOT enter your FQDN\e[21m, refer below example."
+echo -e "\e[30mIf your FQDN is 'www.example.com'. Your domain name would be 'example'."
+echo ": "
 read dName
 
-sudo bash -c 'echo "127.0.0.1 '${dName}'" >> /etc/hosts && exit'
-exitfn
+if grep --quiet -e "127.0.0.1 '${dName}'" /etc/hosts; then
+	echo
+	echo -e "\e[32mHost entry already done.\e[30m"
+	echo
+	sleep 2
+else
+	sudo bash -c 'echo "127.0.0.1 '${dName}'" >> /etc/hosts && exit'
+	exitfn
+fi
 
 cp /etc/nginx/sites-available/default /tmp/default.tmp
-sed -i -e "s/server_name _;/server_name $dName;/" /tmp/default.tmp
-sed -i -e "s/index.html/index.html index.php/" /tmp/default.tmp
-sed -i -e "s#/var/www/html;#/var/www/html/wordpress;#" /tmp/default.tmp
+sed -i -e "0,/.*server_name.*/s/.*server_name.*/        server_name $dName;/" /tmp/default.tmp
+if grep --quiet -e 'index.html index.php' /etc/nginx/sites-available/default; then
+	echo > /dev/null
+else
+	sed -i -e "s/index.html/index.html index.php/" /tmp/default.tmp
+fi
+sed -i -e "s#$nginx_home;#$wp;#" /tmp/default.tmp
+
+if grep --quiet -e 'try_files $uri =404;' /etc/nginx/sites-available/default; then
+	echo
+	echo -e "\e[32mnginx already configured. Continuing...\e[30m"
+	echo
+	sleep 2
+else
 echo "$(ed -s /tmp/default.tmp << eof
 66i
 ${php}
@@ -80,17 +101,19 @@ ${php}
 wq
 eof
 )"
+fi
 sudo mv /tmp/default.tmp /etc/nginx/sites-available/default
 
 if hash wget 2> /dev/null; then
         echo
-        echo "wget Installed. Continuing..."
+        echo -e "\e[32mwget Installed. Continuing...\e[30m"
         echo
+	sleep 1
         wget -c $wp_latest -O $wp_opt
 	exitfn
 else
         echo
-        echo "wget not found. Installing..."
+        echo -e "\e[31mwget not found. Installing...\e[30m"
         echo
         sudo apt-get install wget
         wget $wp_latest -O $wp_opt
@@ -100,43 +123,46 @@ fi
 
 if hash unzip 2> /dev/null; then
         echo
-        echo "unzip Installed. Continuing..."
+        echo -e "\e[32munzip Installed. Continuing...\e[30m"
         echo
-        sudo unzip -a $wp_opt -d $nginx_home
+	sleep 1
+        sudo unzip -a $wp_opt -d $nginx_home"/"
 	exitfn
 else
         echo
-        echo "unzip not found. Installing..."
+        echo -e "\e[31munzip not found. Installing...\e[30m"
         echo
         sudo apt-get install unzip
-        sudo unzip -a $wp_opt -d $nginx_home
+        sudo unzip -a $wp_opt -d $nginx_home"/"
 	exitfn
 fi
 
 db="${dName}_db"
 cmd="create database $db"
-mysql -uroot -h localhost -e "${cmd}"
+[[ -e /var/lib/mysql/$db ]] && echo -e '\e[32m'"MySQL Database already created. Continuing...\e[30m" || mysql -uroot -h localhost -e "${cmd}"
 exitfn
-echo "$(sudo sed -i -e s/database_name_here/$db/g $wp"wp-config-sample.php")"
-echo "$(sudo sed -i -e s/username_here/root/g $wp"wp-config-sample.php")"
-echo "$(sudo sed -i -e s/password_here//g $wp"wp-config-sample.php")"
-sudo mv $wp"wp-config-sample.php" $wp"wp-config.php"
+echo "$(sudo sed -i -e s/database_name_here/$db/g $wp"/wp-config-sample.php")"
+echo "$(sudo sed -i -e s/username_here/root/g $wp"/wp-config-sample.php")"
+echo "$(sudo sed -i -e s/password_here//g $wp"/wp-config-sample.php")"
+sudo mv $wp"/wp-config-sample.php" $wp"/wp-config.php"
 echo
-echo -e '\E[31m'"Php is interfacing with MySQL by root account and no root password is set. Consider using another account and setting a password for the account being used."
-echo -e '\E[31m' "You will need to change the password at line 121(vi +121 wp-nginx.sh) for USER and line 122(vi +122 wp-nginx.sh) for PASSWORD."
+echo -e "\e[31mPhp is interfacing with MySQL by root account and no root password is set. Consider using another account and setting a password for the account being used.\n"
+echo -e "You will need to change the password at line 121(vi +121 wp-nginx.sh) for USER and line 122(vi +122 wp-nginx.sh) for PASSWORD.\e[30m"
+sleep 3
 ###
-sudo chown -R www-data:www-data /var/www/html/
+sudo chown -R www-data:www-data ${nginx_home}"/" 
 ###
 rm -f $wp_opt
-echo "Restarting webserver"
+echo -e "\e[34mRestarting webserver\e[30m"
 sleep 1
-service nginx restart
+sudo service nginx restart
+exitfn
 if (( $? )); then
-echo
-echo "$msg"
-echo
-exit 1
+	echo
+	echo -e "\e[31m$msg\e[30m"
+	echo
+	exit 1
 else
-echo "Everything done. You can now test the running site on a web browser by going to 127.0.0.1"
-exit 0
+	echo -e "\e[32mEverything done. You can now test the running site on a web browser by going to 127.0.0.1\e[30m"
+	exit 0
 fi
